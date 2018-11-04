507dc969d06a2874c33e7355b1bb948b53c07254
Matheus Plata <manpfcb@gmail.com>
2018-11-04 19:26:18 -0200

    Ionic filme

---
+++ myApp/src/pages/home/home.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { LoadingController } from 'ionic-angular';
+import { AboutPage } from '../about/about';
+import { FilmeSalvoPage } from '../filme-salvo/filme-salvo';
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  constructor(public navCtrl: NavController, public loadingCtrl: LoadingController) {
+  }
+
+  presentLoading() {
+    this.loadingCtrl.create({
+      content: 'Carregando, aguarde',
+      duration: 1500,
+      dismissOnPageChange: true
+    }).present();
+
+    this.navCtrl.push(FilmeSalvoPage);
+  }
+}
